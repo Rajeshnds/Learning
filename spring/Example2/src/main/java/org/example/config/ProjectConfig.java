a213ba56a419393494cfbd90c971ab97e4d00ed1
Rajesh Kumar <rajesh.kumar3@publicissapient.com>
2025-10-23 05:39:22 +0530

    multiple bean with same class

---
+++ spring/Example2/src/main/java/org/example/config/ProjectConfig.java
@@ -0,0 +1,29 @@
+package org.example.config;
+import org.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Vehicle vehicle1(){
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle2(){
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle3(){
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
