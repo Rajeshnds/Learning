c0826c3c98035d890bbd3fe71a6cdca1d7d0c6a1
Rajesh Kumar <rajesh.kumar3@publicissapient.com>
2025-11-07 21:34:13 +0530

    adding section 2

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String sayHello(){
+        return "Hello World Everyone!";
+    }
+
+    @GetMapping("sayNo")
+    public String sayNoHello(){
+        return "No Hello!";
+    }
+}
